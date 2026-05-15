2bc59cce713147301c66932284855ab25ad5fd50
rezaanaksoleh213-boop <rezaanaksoleh213@gmail.com>
2026-05-15 15:02:30 +0700

    Add Batu struct and related function declarations

--- header.h
+++ header.h
@@ -1,2 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int h1, h2, v1, v2;
+} Batu;
+
+int V, H, N;   
+Batu *batu;    
+
+int readInput();
+
+int catatan[30][30]; 
+int hitung_total_tabrakan(int v, int h, int V_max, int H_max, Batu *daftar_batu);
 
